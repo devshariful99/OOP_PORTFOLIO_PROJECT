1e8aca61df1c36c7e1f7873bc1f80836d3959700
Md Shariful Islam <116838065+devshariful99@users.noreply.github.com>
2025-03-14 11:56:39 +0600

    user crud --done

--- App/Controllers/UserController.php
+++ App/Controllers/UserController.php
@@ -28,8 +28,7 @@ class UserController
 
     public function index()
     {
-        $stmt = $this->userModel->users();
-        $users = $stmt->fetchAll(\PDO::FETCH_ASSOC);
+        $users = $this->userModel->users();
         require_once "../App/Views/User/index.php";
     }
 
@@ -58,6 +57,10 @@ class UserController
 
             if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
                 $errors[] = "Invalid email format.";
+            }
+            $existingUser = $this->userModel->checkEmailUnique($email);
+            if ($existingUser) {
+                $errors[] = "Email is already taken.";
             }
 
             if (empty($password)) {
@@ -91,11 +94,80 @@ class UserController
         }
     }
 
-    // public function destroy()
-    // {
-    //     $this->userModel->id = $_GET['id'];
-    //     if ($this->userModel->delete()) {
-    //         header("Location: /");
-    //     }
-    // }
+    public function edit($id)
+    {
+        $this->userModel->id = $id;
+        $user = $this->userModel->user();
+        require_once "../App/Views/User/edit.php";
+    }
+
+
+    public function update($id)
+    {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            // Sanitize input
+            $email = filter_var(trim($_POST['email']), FILTER_SANITIZE_EMAIL);
+            $name = trim($_POST['name']);
+            $password = trim($_POST['password']);
+            $confirm_password = trim($_POST['confirm_password']);
+            $errors = [];
+
+            // Validation checks
+            if (empty($name)) {
+                $errors[] = "Name field is required.";
+            }
+            if (empty($email)) {
+                $errors[] = "Email field is required.";
+            }
+
+            if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                $errors[] = "Invalid email format.";
+            }
+            $existingUser = $this->userModel->checkEmailUnique($email, $id);
+            if ($existingUser) {
+                $errors[] = "Email is already taken.";
+            }
+            if (!empty($password) && strlen($password) < 6) {
+                $errors[] = "Password must be at least 6 characters long.";
+            }
+            if (!empty($password) && $password !== $confirm_password) {
+                $errors[] = "Passwords do not match.";
+            }
+
+
+
+            if (!empty($errors)) {
+                $_SESSION['errors'] = $errors;
+                header("Location: /user/edit/$id");
+            } else {
+                $this->userModel->id = $id;
+                $user = $this->userModel->user();
+                if ($user['id']) {
+                    $this->userModel->email = $email;
+                    $this->userModel->name = $name;
+                    $this->userModel->password = $password ? password_hash($password, PASSWORD_DEFAULT) : $user['password'];
+                    if ($this->userModel->update()) {
+                        header("Location: /user/index");
+                    }
+                } else {
+                    header("Location: /user/index");
+                }
+            }
+        }
+    }
+
+    public function status($id)
+    {
+        $this->userModel->id = $id;
+        if ($this->userModel->status()) {
+            header("Location: /user/index");
+        }
+    }
+    public function delete($id)
+    {
+        $this->userModel->id = $id;
+        if ($this->userModel->delete()) {
+            header("Location: /user/index");
+        }
+    }
 }
